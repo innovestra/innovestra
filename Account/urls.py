467f0a91a05436756a3185f11c2b5778a430c6e5
innovestra <innovetsra05t@gmail.com>
2025-10-09 01:40:37 +0300

    Homepage Setup

--- Account/urls.py
+++ Account/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('account/', views.register, name='register'),
+    path('account/register/', views.registeruser, name='register'),
+    path('account/login/', views.loginuser, name='login'),
+    path('account/forgot-password/', views.forgotpassword, name='forgot_password'),
 ]
--- Account/views.py
+++ Account/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-def register(request):
+def registeruser(request):
     return render(request, 'account/register.html')
+
+def loginuser(request):
+    return render(request, 'account/login.html')
+
+def forgotpassword(request):
+    return render(request, 'account/forgot_password.html')
