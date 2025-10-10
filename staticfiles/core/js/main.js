5cd35476687b41436b45ed7660cdceb23017c096
innovestra <innovetsra05t@gmail.com>
2025-10-11 01:56:00 +0300

    Auth System Completed

---
+++ staticfiles/core/js/main.js
@@ -0,0 +1,21 @@
+
+// function showAlert(icon, title, text) {
+//   Swal.fire({
+//     icon: icon,
+//     title: title,
+//     text: text,
+//     confirmButtonColor: '#30b54a',
+//   });
+// }
+
+// function showToast(icon, title) {
+//   Swal.fire({
+//     toast: true,
+//     position: 'bottom-end',
+//     icon: icon,
+//     title: title,
+//     showConfirmButton: false,
+//     timer: 3000,
+//     timerProgressBar: true,
+//   });
+// }
